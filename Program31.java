ac67060cac5958b7f386ef7eeaa9435cb4da6289
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-03 13:29:25 +0530

    Program 31 package in java

---
+++ Program31.java
@@ -0,0 +1,12 @@
+import myPackage.MyClass;
+
+public class Program31 {
+
+    public static void main(String[] args) {
+        System.out.println("Package In Java");
+        String name="Suraj Singh";
+
+        MyClass obj=new MyClass();
+        obj.getNames(name);
+    }
+}
