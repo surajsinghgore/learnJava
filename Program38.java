ccb14cd8602f32d6451c81075fb7135dd66407f2
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-03 16:12:27 +0530

    Program 38 Applet Text

---
+++ Program38.java
@@ -0,0 +1,14 @@
+//First.java  
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class Program38 extends Applet{  
+  
+public void paint(Graphics g){  
+g.drawString("welcome to applet",70,70);  
+}  
+  
+}  
+/* 
+<applet code="Program38.class" width="400" height="400"> 
+</applet> 
+*/  
