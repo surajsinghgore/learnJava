5ecc2985b6bd8afe58211b6dabc875a65e05693a
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-03 17:09:55 +0530

    Program42 polygon

---
+++ Program42.java
@@ -0,0 +1,17 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Program42 extends Applet{
+    
+    public void paint(Graphics g) {
+
+        g.drawLine(180,150,180,370);
+g.drawLine(180,150,440,370);
+g.drawLine(180,370,440,370);
+    }
+
+}
+/*
+ * <applet code="Program42.class" width="500" height="500">
+ * </applet>
+ */
