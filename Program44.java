dc583d694ce21d36f628707134a787b185dad55c
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-05 10:05:01 +0530

    Program 44 FlowLayout Update

--- Program44.java
+++ Program44.java
@@ -2,14 +2,14 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Program44 extends Applet {
-    Label lb1 = new Label("FlowLayout");
+    Label lb = new Label("FlowLayout");
     FlowLayout fl = new FlowLayout(FlowLayout.RIGHT);
-    Button btn1 = new Button( "RIGHT");
+    Button btn = new Button( "RIGHT");
 
     public void init() {
-        add(lb1);
+        add(lb);
         setLayout(fl);
-        add(btn1);
+        add(btn);
   
 
     }
