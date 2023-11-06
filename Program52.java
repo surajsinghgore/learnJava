f02185fbcfcb24e2ea28baa735aa79e25caab4f6
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-06 19:42:51 +0530

    Program52 update

---
+++ Program52.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Program52 {
+    public static void main(String[] args) {
+        String URl = "jdbc:mysql://localhost:3306/my_db";
+        String username = "root";
+        String password = "password";
+        try {
+            Connection con = DriverManager.getConnection(URl, username, password);
+            Statement stm = con.createStatement();
+
+            // create table
+            String query = "update student set name='rahul' where id=1";
+            stm.execute(query);
+            System.out.println("Table Record Update Successfully");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
