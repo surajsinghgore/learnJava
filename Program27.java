e17e2e464254fbdf5cc1461537e4c5e1c076854a
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 16:49:10 +0530

    Program27 one dimensional array

---
+++ Program27.java
@@ -0,0 +1,15 @@
+public class Program27 {
+    public static void main(String[] args) {
+        int arr[];
+        arr=new int[5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        System.out.println("One Dimensional Array");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("arr["+i+" ] = "+arr[i]);
+        }
+    }
+}
