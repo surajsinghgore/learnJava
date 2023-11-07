1c9923195ff98fb0d379c1ab7f8d16223ae086e5
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 08:47:25 +0530

    Program6 switch statment

---
+++ Program6.java
@@ -0,0 +1,20 @@
+public class Program6 {
+    public static void main(String[] args) {
+        int num = 2;
+        switch (num) {
+            case 1:
+                System.out.println("case 1 is selected");
+                break;
+            case 2:
+                System.out.println("case 2 is selected");
+                break;
+            case 3:
+                System.out.println("case 3 is selected");
+                break;
+
+            default:
+                System.out.println("Invalid Input");
+                break;
+        }
+    }
+}
