315d4988c11dd5c6fc0dee296291fc46b3fd8ef7
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 15:48:35 +0530

    Program12 Default constructor

---
+++ Program12.java
@@ -0,0 +1,11 @@
+public class Program12 {
+    Program12(){
+        System.out.println("Program12 is created");
+    }
+    public static void main(String[] args) {
+        System.out.println("Default Constructor");
+        Program12 obj=new Program12();
+        System.out.println("default constructor automatic created");
+
+    }
+}
