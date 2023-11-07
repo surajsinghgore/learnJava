25151791f31ec18d4c8c897373e39874a5d15f0d
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 08:42:37 +0530

    Program4 Operators

---
+++ Program4.java
@@ -0,0 +1,12 @@
+public class Program4 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("A="+a+" B="+b);
+        System.out.println("Addition= "+(a+b));
+        System.out.println("Subtract= "+(a-b));
+        System.out.println("Multiple= "+(a*b));
+        System.out.println("Divide= "+(a/b));
+        System.out.println("Remainder= "+(a%b));
+    }
+}
