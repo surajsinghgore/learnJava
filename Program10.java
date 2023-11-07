f7cb2b625a7aa4866a29f463a4013c0095b4cd4b
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 15:39:55 +0530

    Program10 class and object

---
+++ Program10.java
@@ -0,0 +1,15 @@
+class student{
+int rollNo=2341;
+    String name="Suraj Singh";
+
+}
+public class Program10 {
+    
+    public static void main(String[] args) {
+        student obj=new student();
+        System.out.println("Class And Object");
+        System.out.println("Class Created");
+        System.out.println("Roll No = "+obj.rollNo);
+        System.out.println("Name = "+obj.name);
+    }
+}
