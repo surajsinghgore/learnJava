214ecd895b8238d48a17ded04874a89285016a3e
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 15:35:07 +0530

    Program8 while loop

---
+++ Program8.java
@@ -0,0 +1,10 @@
+public class Program8 {
+    public static void main(String[] args) {
+        System.out.println("While Loop");
+       int i=1;
+        while (i<=10) {
+        System.out.print(i+" ");
+        i++;
+       }
+    }
+}
