966536ea35b71f2be33a270f8de0506f0bad4a5c
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 16:07:15 +0530

    Program16 method in java

---
+++ Program16.java
@@ -0,0 +1,17 @@
+public class Program16 {
+    public void findEvenOdd(int num){
+        if(num%2==0){
+System.out.println(num+" is even");
+        }
+        else{
+            System.out.println(num+" is odd");
+        }
+    }
+
+    public static void main(String[] args) {
+       int num=20;
+       System.out.println("Method In Java");
+       Program16 obj=new Program16();
+       obj.findEvenOdd(num); 
+    }
+}
