73042bfa6e99a78b1309b4c510c7173045f6e3d2
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 15:33:21 +0530

    Program7 For Loop

---
+++ Program7.java
@@ -0,0 +1,8 @@
+public class Program7 {
+    public static void main(String[] args) {
+        System.out.println("For Loop");
+        for (int i = 1; i <=10; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
