2afaab2c558fa7c8705d74ada93a0c80c5142fba
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 16:36:59 +0530

    Program24 final keyword

---
+++ Program24.java
@@ -0,0 +1,8 @@
+public class Program24 {
+   final int num=100;
+   public static void main(String[] args) {
+    System.out.println("Final Keyword");
+    Program24 obj=new Program24();
+    System.out.println("Num="+obj.num);
+   } 
+}
