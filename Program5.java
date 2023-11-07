985f0ac0eb7f7e812638089cb29472ba65c5496f
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 08:44:58 +0530

    Program5 If-else statement

---
+++ Program5.java
@@ -0,0 +1,12 @@
+public class Program5 {
+    public static void main(String[] args) {
+        System.out.println("If-Else decision Statement");
+        int age=30;
+        if(age>=18){
+            System.out.println("You are 18 Above");
+        }
+        else{
+            System.out.println("You are 18 Below");
+        }
+    }
+}
