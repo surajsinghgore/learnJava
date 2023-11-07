34b337ecfe5a9193ffa26fd0eeb03b1be21f48cc
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-07 15:36:28 +0530

    Program9 while

---
+++ Program9.java
@@ -0,0 +1,11 @@
+public class Program9 {
+    public static void main(String[] args) {
+        System.out.println("Do-While Loop");
+       int i=1;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }
+        while(i<=10);
+    }
+}
