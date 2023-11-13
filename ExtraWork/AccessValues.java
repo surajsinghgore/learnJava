6b910263ed05f6a0ab6843ef12cbfbd55e9f019d
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-13 20:53:24 +0530

    Extra work

---
+++ ExtraWork/AccessValues.java
@@ -0,0 +1,8 @@
+import c4.sample.SampleClass;
+public class AccessValues extends SampleClass{
+    public static void main(String[] args) {
+        AccessValues obj=new AccessValues();
+        obj.protectedMethod();
+ 
+    }
+}
---
+++ ExtraWork/c4/sample/SampleClass.java
@@ -0,0 +1,11 @@
+package c4.sample;
+
+public class SampleClass {
+    protected int protectedVar = 20;
+    protected void protectedMethod() {
+        System.out.println("Protected Method");
+        System.out.println("Protected Var = " + protectedVar);
+    }
+}
+
+
