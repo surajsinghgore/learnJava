1fc1eeb0c9fd2101050c83c52b7576c67f80f977
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-20 17:20:25 +0530

    formattings

--- Database/CreateDb.java
+++ Database/CreateDb.java
@@ -5,20 +5,19 @@ import java.sql.Statement;
 
 public class CreateDb {
    public static void main(String[] args) {
-    String URl="jdbc:mysql://localhost:3306/";
-    String username="root";
-    String password="password";
-    try {
-    Connection con=DriverManager.getConnection(URl, username, password);
-    Statement stm=con.createStatement();
+      String URl = "jdbc:mysql://localhost:3306/";
+      String username = "root";
+      String password = "password";
+      try {
+         Connection con = DriverManager.getConnection(URl, username, password);
+         Statement stm = con.createStatement();
 
-    //query to create
-    String query="create database myDatabase";
-    stm.execute(query);
-    System.out.println("Database created Successfully");
-} 
-catch (Exception e) {
-   System.out.println(e);
-}
-   } 
+         // query to create
+         String query = "create database myDatabase";
+         stm.execute(query);
+         System.out.println("Database created Successfully");
+      } catch (Exception e) {
+         System.out.println(e);
+      }
+   }
 }
