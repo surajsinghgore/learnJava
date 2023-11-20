c44833a2d8c8642b54e7ce3bc8f97ad04c3d5b67
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-20 16:55:08 +0530

    Write String in applet

---
+++ Applet/PrintString.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class PrintString extends Applet{
+
+    public void paint(Graphics g){
+        g.drawString("Hello World", 100, 200);
+    }
+}
+
+/* <applet code="PrintString.class" height="400" width="500"></applet> */
