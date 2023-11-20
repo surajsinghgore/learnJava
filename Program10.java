78d183cf985aade0d4de1f0839734ead60f0b9c6
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-20 17:21:05 +0530

    formattings

--- Program10.java
+++ Program10.java
@@ -1,15 +1,16 @@
-class student{
-int rollNo=2341;
-    String name="Suraj Singh";
+class student {
+    int rollNo = 2341;
+    String name = "Suraj Singh";
 
 }
+
 public class Program10 {
-    
+
     public static void main(String[] args) {
-        student obj=new student();
+        student obj = new student();
         System.out.println("Class And Object");
         System.out.println("Class Created");
-        System.out.println("Roll No = "+obj.rollNo);
-        System.out.println("Name = "+obj.name);
+        System.out.println("Roll No = " + obj.rollNo);
+        System.out.println("Name = " + obj.name);
     }
 }
--- Program14.java
+++ Program14.java
@@ -1,29 +1,33 @@
-class Student{
+class Student {
 
-    int id;String name;
-Student(int i,String n){
-id=i;
-name=n;
-}
-Student(Student obj){
-    id=obj.id;
-    name=obj.name;
-}
-void display(){
-    System.out.println("Id = "+id+" Name = "+name);
-}
+    int id;
+    String name;
+
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    Student(Student obj) {
+        id = obj.id;
+        name = obj.name;
+    }
+
+    void display() {
+        System.out.println("Id = " + id + " Name = " + name);
+    }
 }
 
 public class Program14 {
     public static void main(String[] args) {
-        Student obj1=new Student(11,"suraj");
-        Student obj2=new Student(12,"shubham");
+        Student obj1 = new Student(11, "suraj");
+        Student obj2 = new Student(12, "shubham");
         System.out.println("Copy Constructor");
         obj1.display();
         obj2.display();
-        Student obj3=new Student(obj1);
-obj3.id=obj1.id;
-obj3.name=obj1.name;
-obj3.display();
+        Student obj3 = new Student(obj1);
+        obj3.id = obj1.id;
+        obj3.name = obj1.name;
+        obj3.display();
     }
 }
